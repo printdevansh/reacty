aa9c766815e439590925afdbf3e27f0c641f0c99
Devansh Srivastava <devanshsri957@gmail.com>
2024-09-02 00:42:53 +0530

    up

---
+++ hello/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>
+//         Hello Devansh
+//     </h1>
+// }
+
+const Greet=()=> <h1>Hello Devansh</h1>
+
+export default Greet
